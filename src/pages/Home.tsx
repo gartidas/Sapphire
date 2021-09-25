9bc3a6285e94d2c01f330444d0991d299149a3d1
Adam.Harin <Adam.Harin@quest.com>
2021-09-25 22:22:33 +0200

    Tab online indicator

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 
 import LoggedLayout from "../components/layouts/LoggedLayout";
 import HomeTemplate from "../components/templates/Home/HomeTemplate";
+import { useAuth } from "../contextProviders/AuthProvider";
 
 function Home() {
+  const auth = useAuth();
+  const titles: string[] = ["◇ Sapphire ◇ Home", "◆ Sapphire ◆ Home"];
+  const [titleVersion, setTitleVersion] = useState(0);
+
+  useEffect(() => {
+    if (auth.isOnline) {
+      const interval = setInterval(() => {
+        if (titleVersion === 0) setTitleVersion(1);
+        else setTitleVersion(0);
+      }, 2000);
+      return () => clearInterval(interval);
+    }
+  }, [auth, titleVersion]);
+
   return (
     <LoggedLayout>
-      <Helmet>
-        <title>◇ Sapphire ◇ Home</title>
+      <Helmet defer={false}>
+        <title>{titles[titleVersion]}</title>
       </Helmet>
       <HomeTemplate />
     </LoggedLayout>
