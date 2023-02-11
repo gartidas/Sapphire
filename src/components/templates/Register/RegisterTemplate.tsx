3b5c083c0370c1952579adef30f9fe7e2b5344ad
Adam.Harin <Adam.Harin@quest.com>
2023-02-11 18:48:23 +0100

    Removed share link text

--- src/components/templates/Register/RegisterTemplate.tsx
+++ src/components/templates/Register/RegisterTemplate.tsx
@@ -92,7 +92,7 @@ const RegisterTemplate = () => {
                 <Email />
               </SocialIcon>
               <SocialIcon
-                href={`http://www.facebook.com/dialog/send?app_id=${commonConfig.facebookAppId!}&link=${`${shareUrl}/${user.familyId}`}&redirect_uri=${`${shareUrl}/${user.familyId}`}`}
+                href={`http://www.facebook.com/dialog/send?app_id=${commonConfig.facebookAppId!}&href=${`${shareUrl}/${user.familyId}`}&redirect_uri=${`${shareUrl}/${user.familyId}`}`}
                 target="_blank"
               >
                 <FacebookMessengerIcon />
@@ -118,7 +118,6 @@ const RegisterTemplate = () => {
                 onClick={() => {
                   navigator.share({
                     url: `${shareUrl}/${user.familyId}`,
-                    text: "Come and join my family in Sapphire app!",
                   });
                 }}
               >
