5aecf632aef156ebfb66f9815f10a9aa4b1085bf
Adam.Harin <Adam.Harin@quest.com>
2025-01-25 20:24:46 +0100

    Fix weak password message

--- src/helpers/firebaseErrorToFieldError/config/changePasswordErrorConfig.ts
+++ src/helpers/firebaseErrorToFieldError/config/changePasswordErrorConfig.ts
@@ -4,4 +4,5 @@ export const changePasswordErrorConfig: Partial<
   Record<string, keyof IChangePasswordData>
 > = {
   "auth/wrong-password": "currentPassword",
+  "auth/weak-password": "newPassword",
 };
